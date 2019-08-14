c72001d3a1c215a6185af7f9058bc41f611fac3b
Michael Withers <wiseguy@Michaels-MacBook-Air.local>
2019-08-14 09:45:18 -0400

    new commit

---
+++ src/main/java/com/example/coffeeandtequila/CoffeeAndTequilaApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeandtequila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeAndTequilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeAndTequilaApplication.class, args);
+    }
+
+}
