709b549243c01b2b9799b941ca3ed11f08b0255d
Tiquila McNeill <tiquilamceille@gmail.com>
2019-08-23 12:11:17 -0400

    some jsx changes

--- coffeefe/src/App.js
+++ coffeefe/src/App.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './SignIn';
 import About from './About';
-import Coffeeshops from "./Coffeeshops";
+import CoffeeShops from "./CoffeeShops";
 
 
 
@@ -12,34 +11,14 @@ class App extends Component{
         return(
             <Router>
             <Switch>
-            <Route path='/signin' exact={true} component={SignIn}/>
+            <Route path='/' exact={true} component={SignIn}/>
             <Route path='/about' exact={true} component={About}/>
-            <Route path='/coffeeshops' exact={true} component={Coffeeshops}/>
+            <Route path='/coffeeshops' exact={true} component={CoffeeShops}/>
             </Switch>
             </Router>
 
         )
     }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
--- coffeefe/src/SignIn.js
+++ coffeefe/src/SignIn.js
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 //import './src/App.js';
 
 
- function SignIn() {
+ class SignIn extends Component{
+     render() {
          return (
              <div style={{background: '#3e1e04'}}>
-              <div className="container" >
-                 <div className="jumbotron" style={{background: '#40e0d0'}}>
-                     <h1>Coffee and Tiquila</h1></div>
-                 <form className="form-signin" method="post">
-                     <h3 className="form-signin-heading" style={{textAlign: 'center', color: '#40e0d0'}}>Please Sign
-                         In</h3>
-                     <p>
-                         <label htmlFor="username" className="sr-only">Username</label>
-                         <input style={{background: '#40e0d0', border: '#40e0d0'}} type="text" id="username"
-                                name="username" className="form-control" placeholder="Username"/>
-                     </p>
-                     <p>
-                         <label htmlFor="password" className="sr-only">Password</label>
-                         <input style={{background: '#40e0d0', border: '#40e0d0'}} type="password" id="password"
-                                name="password" className="form-control" placeholder="Password"/>
-                     </p>
-                     <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                         {/*Sign In Button*/}
-                         <div style={{width: '50%', alignContent: 'flex-start'}}>
-                             <button onClick={SignIn} className="btn btn-sm " name="submit" type="submit" style={{
-                                 width: '96%',
-                                 marginRight: '3%',
-                                 flexGrow: '0.45',
-                                 background: '#40e0d0'
-                             }}>Sign In
-                             </button>
-                         </div>
-                         {/*Sign Up (Redirect) Button*/}
-                         <div style={{width: '50%', alignContent: 'flex-end'}}>
-                             <form method="get">
-                                 <button onClick={SignUp} className="btn btn-sm " name="Submit" type="Submit" style={{
+                 <div className="container">
+                     <div className="jumbotron" style={{background: '#40e0d0'}}>
+                         <h1>Coffee and Tiquila</h1></div>
+                     <form className="form-signin" method="post">
+                         <h3 className="form-signin-heading" style={{textAlign: 'center', color: '#40e0d0'}}>Please Sign
+                             In</h3>
+                         <p>
+                             <label htmlFor="username" className="sr-only">Username</label>
+                             <input style={{background: '#40e0d0', border: '#40e0d0'}} type="text" id="username"
+                                    name="username" className="form-control" placeholder="Username"/>
+                         </p>
+                         <p>
+                             <label htmlFor="password" className="sr-only">Password</label>
+                             <input style={{background: '#40e0d0', border: '#40e0d0'}} type="password" id="password"
+                                    name="password" className="form-control" placeholder="Password"/>
+                         </p>
+                         <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                             {/*Sign In Button*/}
+                             <div style={{width: '50%', alignContent: 'flex-start'}}>
+                                 <button onClick={SignIn} className="btn btn-sm " name="submit" type="submit" style={{
                                      width: '96%',
-                                     marginLeft: '3%',
+                                     marginRight: '3%',
                                      flexGrow: '0.45',
                                      background: '#40e0d0'
-                                 }}>Sign Up
+                                 }}>Sign In
                                  </button>
-                             </form>
+                             </div>
+                             {/*Sign Up (Redirect) Button*/}
+                             <div style={{width: '50%', alignContent: 'flex-end'}}>
+                                 <form method="get">
+                                     <button onClick={SignUp} className="btn btn-sm " name="Submit" type="Submit"
+                                             style={{
+                                                 width: '96%',
+                                                 marginLeft: '3%',
+                                                 flexGrow: '0.45',
+                                                 background: '#40e0d0'
+                                             }}>Sign Up
+                                     </button>
+                                 </form>
+                             </div>
                          </div>
-                     </div>
-                 </form>
-             </div>
-             </div>
+                     </form>
+                 </div>
+             </div>git
          );
+     }
  }
 
 // class SignIn extends Component{
